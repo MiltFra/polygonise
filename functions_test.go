f0178bd7f14b768ba3130a0f870ac67318f16418
Franz Miltz <franz@miltz.me>
2019-06-20 21:53:18 +0200

    Added a test for the Get function

--- functions_test.go
+++ functions_test.go
@@ -1,3 +1,19 @@
 package polygonize
 
-//func Test
+import "testing"
+
+func TestGet(t *testing.T) {
+	img, err := FromFile("test/rectangle_test.png")
+	if err != nil {
+		t.Fail()
+	}
+	f, err := NewGreyFilter(100, true)
+	p := Get(img, f, 100)
+	if len(p) != 1 || len(p[0]) < 4 {
+		t.Fail()
+	}
+	p[0] = Flatten(p[0])
+	if len(p[0]) < 4 || len(p[0])/2 > 8 {
+		t.Fail()
+	}
+}
