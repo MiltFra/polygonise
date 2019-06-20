aba47c8e3340e5fae9d32bc2bcef2f88f9fc03fe
Franz Miltz <franz@miltz.me>
2019-06-20 20:22:34 +0200

    Added some tests

---
+++ convert_test.go
@@ -0,0 +1,30 @@
+package polygonize
+
+import (
+	"image"
+	"image/color"
+	"testing"
+)
+
+func TestToRGBA(t *testing.T) {
+	bounds := image.Rect(0, 0, 1000, 1000)
+	img := image.NewRGBA(bounds)
+	for i := 0; i < 1000000; i++ {
+		img.SetRGBA(i/1000, i%1000, color.RGBA{uint8((i / 1000) / 4), uint8((i % 1000) / 4), 0, 0})
+	}
+	img2 := ToRGBA(img)
+	for i := 0; i < 1000000; i++ {
+		col := color.RGBA{uint8((i / 1000) / 4), uint8((i % 1000) / 4), 0, 0}
+		if img2.At(i/1000, i%1000) != col {
+			t.Fail()
+		}
+	}
+}
+
+func TestFromFile(t *testing.T) {
+	img, err := FromFile("test/rectangle_test.png")
+	if err != nil {
+		t.Fail()
+	}
+	_ = ToRGBA(img)
+}
---
+++ functions_test.go
@@ -0,0 +1,3 @@
+package polygonize
+
+//func Test
