e29e89e9f754b90dc109138f5dc45db20d5f2d6d
Franz Miltz <franz@miltz.me>
2019-06-21 12:03:29 +0200

    Extended filter tests to a real image

--- filters_test.go
+++ filters_test.go
@@ -53,6 +53,23 @@ func TestGreenFilter(t *testing.T) {
 		t.Fail()
 	}
 }
+func TestGreenFilterByImage(t *testing.T) {
+	img, err := FromFile("test/rectangle_test.png")
+	if err != nil {
+		t.Fail()
+	}
+	f, err := NewGreenFilter(100, true)
+	if err != nil {
+		t.Fail()
+	}
+	if !f.Filter(img.RGBAAt(1850, 63)) ||
+		!f.Filter(img.RGBAAt(1850, 64)) {
+		t.Fail()
+	}
+	if f.Filter(img.RGBAAt(0, 0)) {
+		t.Fail()
+	}
+}
 
 func TestBlueFilter(t *testing.T) {
 	f, err := NewBlueFilter(100, false)
@@ -70,6 +87,25 @@ func TestBlueFilter(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestBlueFilterByImage(t *testing.T) {
+	img, err := FromFile("test/rectangle_test.png")
+	if err != nil {
+		t.Fail()
+	}
+	f, err := NewBlueFilter(100, true)
+	if err != nil {
+		t.Fail()
+	}
+	if !f.Filter(img.RGBAAt(1850, 63)) ||
+		!f.Filter(img.RGBAAt(1850, 64)) {
+		t.Fail()
+	}
+	if f.Filter(img.RGBAAt(0, 0)) {
+		t.Fail()
+	}
+}
+
 func TestRedFilter(t *testing.T) {
 	f, err := NewRedFilter(100, false)
 	if err != nil {
@@ -83,6 +119,23 @@ func TestRedFilter(t *testing.T) {
 		t.Fail()
 	}
 	if f.Filter(f.FalseValue()) || !f.Filter(f.TrueValue()) {
+		t.Fail()
+	}
+}
+func TestRedFilterByImage(t *testing.T) {
+	img, err := FromFile("test/rectangle_test.png")
+	if err != nil {
+		t.Fail()
+	}
+	f, err := NewRedFilter(100, true)
+	if err != nil {
+		t.Fail()
+	}
+	if !f.Filter(img.RGBAAt(1850, 63)) ||
+		!f.Filter(img.RGBAAt(1850, 64)) {
+		t.Fail()
+	}
+	if f.Filter(img.RGBAAt(0, 0)) {
 		t.Fail()
 	}
 }
