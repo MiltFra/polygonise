40c10a6dc73c2454fe5f45155c6d4b2617fb1239
Franz Miltz <franz@miltz.me>
2019-06-21 13:36:05 +0200

    Added a test for Flatten

--- functions_test.go
+++ functions_test.go
@@ -1,6 +1,8 @@
 package polygonize
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGet(t *testing.T) {
 	img, err := FromFile("test/rectangle_test.png")
@@ -14,6 +16,14 @@ func TestGet(t *testing.T) {
 	}
 	p[0] = Flatten(p[0])
 	if len(p[0]) < 4 || len(p[0])/2 > 8 {
+		t.Fail()
+	}
+}
+
+func TestFlatten(t *testing.T) {
+	p := []int{0, 0, 0, 5, 3, 5, 5, 5, 1, 1}
+	p = Flatten(p)
+	if len(p) != 6 {
 		t.Fail()
 	}
 }
